ac4e494eba0befac834598de4694c0a99aac5c9b
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-06 18:00:53 +0100

    minor changes in main.go

--- main.go
+++ main.go
@@ -21,14 +21,12 @@ func init() {
 }
 
 func main() {
-	var (
-		flDomain      = flag.String("domain", "", "The domain to perform guessing against")
-		flWordlist    = flag.String("wordlist", "", "The wordlist to use for guessing")
-		flWorkerCount = flag.Int("c", 100, "The number of workers to use")
-		flServerAddr  = flag.String("server", "8.8.8.8:53", "The DNS server to use")
-	)
+	flDomain := flag.String("domain", "", "The domain to perform guessing against")
+	flWordlist := flag.String("wordlist", "", "The wordlist to use for guessing")
+	flWorkerCount := flag.Int("c", 100, "The number of workers to use")
+	flServerAddr := flag.String("server", "8.8.8.8:53", "The DNS server to use")
 	flag.Parse()
-	if *flDomain == "" || *flWordlist == "" {
+	if *flDomain == "" || *flWordlist == "" { // Non-optional options :-).
 		log.Fatal("-domain and -wordlist are required")
 	}
 
