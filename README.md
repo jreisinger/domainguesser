f1f73db6348321b9ba846b086a4c6bd8008db96a
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-08 23:11:43 +0100

    minor changes in README.md, main.go

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Domainguesser tries to find subdomains for a given domain. If a subdomain is
+found its IP address is looked up. Stolen from "Black Hat Go: 5 Exploiting
+DNS".
+
 ```
 $ go install
 
--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Domainguesser tries to find subdomains for a given domain. If a subdomain is
-// found its IP address is looked up. Stolen from "Black Hat Go: 5 Exploiting
-// DNS".
 package main
 
 import (
